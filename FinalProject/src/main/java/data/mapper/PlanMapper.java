43ec0581ee64c6ecda029fb82aa0afd0069a715b
jiw3052 <jiw3052@naver.com>
2022-06-30 15:22:24 +0900

    Plan update from woong

--- FinalProject/src/main/java/data/mapper/PlanMapper.java
+++ FinalProject/src/main/java/data/mapper/PlanMapper.java
@@ -1,8 +1,12 @@
 package data.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public class PlanMapper {
+import data.dto.PlanDto;
 
+@Mapper
+public interface PlanMapper {
+	public List<PlanDto> getPlanDatas();
 }
