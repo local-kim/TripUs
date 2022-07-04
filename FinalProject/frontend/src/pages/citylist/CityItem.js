baf985dcdfb0d7d1c9019742d537e0c482ef3d30
김현지 <gumgum28@naver.com>
2022-07-04 16:13:28 +0900

    도시 정보 페이지로 이동하는 내비게이션 추가

--- FinalProject/frontend/src/pages/citylist/CityItem.js
+++ FinalProject/frontend/src/pages/citylist/CityItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/citylist.css';
 
 const CityItem = ({city}) => {
   return (
     <div className='city-item'>
-      <div className='city-image' style={{backgroundImage:`url(../../city_image/${city.image})`}}>{city.eng_name.toUpperCase()}</div>
-      <span>{city.name}, {city.country}</span>
+      <Link to={`/city/${city.num}`}>
+        <div className='city-image' style={{backgroundImage:`url(../../city_image/${city.image})`}}>{city.eng_name.toUpperCase()}</div>
+      </Link>
+      <div className='city-name'>{city.name}, {city.country}</div>
       {/* <img src={`${}`} alt=''/>  */}
     </div>
   );
