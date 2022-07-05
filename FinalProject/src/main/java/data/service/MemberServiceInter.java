ff41b99aa8929d3324e71e57d9138e826ec0c171
boccu <twayong@gmail.com>
2022-07-05 13:26:22 +0900

    sessoin add

--- FinalProject/src/main/java/data/service/MemberServiceInter.java
+++ FinalProject/src/main/java/data/service/MemberServiceInter.java
@@ -1,13 +1,19 @@
 package data.service;
 
+import java.util.List;
+import java.util.Map;
+
 import data.dto.MemberDto;
 
 public interface MemberServiceInter {
 	public void insertMember(MemberDto dto);
 	public String getName(String id);
-	public int loginCheck(String id,String pass);
+//	public int loginCheck(String id,String pass);
 	public void deleteMember(int num);
 	public int idcheck(String id);
 	public int emailcheck(String email);
+	public boolean login(String id, String password);
+	public List<Map<String, Object>> getLoginInfo(String id);
+	public int checkKakaoMember(String id);
 
 }
