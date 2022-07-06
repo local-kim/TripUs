e498acb932411b50d2e5cc0a47f2c22af631e9b0
ddanddan <74584001+ddanddan@users.noreply.github.com>
2022-07-06 14:38:30 +0900

    review data add modal detail & delete

--- FinalProject/src/main/java/data/service/ReviewServiceInter.java
+++ FinalProject/src/main/java/data/service/ReviewServiceInter.java
@@ -7,9 +7,11 @@ import data.dto.ReviewDto;
 public interface ReviewServiceInter {
 	
 	public void insertReview(ReviewDto dto);
+	public void deleteReview(int num);
 	public int getTotalCount();
 	public List<ReviewDto> getPagingList(int start,int perpage);
 	public List<ReviewDto> getAllDatas(String place_id);
 	public double getAvgStars(String place_id);
 	public ReviewDto getData(int num);
+	public void updateReview(ReviewDto dto);
 }
