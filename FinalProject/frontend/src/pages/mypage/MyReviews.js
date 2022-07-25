5a39ee50cd3f91419e998806c0b20cdff6f050ed
김현지 <gumgum28@naver.com>
2022-07-25 16:30:24 +0900

    마이페이지에서 내 리뷰 보기

--- FinalProject/frontend/src/pages/mypage/MyReviews.js
+++ FinalProject/frontend/src/pages/mypage/MyReviews.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
+import { useSelector } from 'react-redux';
 
 const MyReviews = ({reviewList}) => {
   const navigate = useNavigate();
+  const loginNum = useSelector((state) => state.auth.user.num);
+
+  // const [reviewList, setReviewList] = useState([]);
+
+  // let reviewUrl = `${process.env.REACT_APP_SPRING_URL}mypage/myreview?loginNum=${loginNum}`;
+
+  // useEffect(() => {
+  //   axios.get(reviewUrl)
+  //   .then(res => {
+  //     console.log(res.data);
+  //     setReviewList(res.data);
+  //   })
+  //   .catch(err => console.log(err));
+  // }, []);
 
   return (
-    <div>
+    <div id='my-reviews'>
+      <div className="section-title-container">
+        <span className="section-title">
+          나의 리뷰
+        </span>
+      </div>
+
       {
         reviewList.length === 0 ? (
           <div style={{textAlign:'center', color: 'gray', marginTop:'100px'}}>작성한 후기가 없습니다.</div>
